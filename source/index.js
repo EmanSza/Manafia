77403b0a2e588c5352caad1e01b7250f27223ea2
EmanSza <eszabadka@hotmail.ca>
2020-12-10 19:48:20 -0500

    Pushing Basic Shit

--- source/index.js
+++ source/index.js
@@ -1,9 +1,14 @@
 const discord = require('discord.js');
 const mongoose = require('mongoose')
+
 const { TOKEN } = require('../configs/config.json')
+const { registerCommands, registerEvents } = require('./utils/registry');
+
 const client = new discord.Client();
+
 (async () => {
     console.log('Logging in')
     await client.login(TOKEN);
     console.log('Loaded {Empty} and Online!')
+    client.commands = new discord.Collection();
 })();
